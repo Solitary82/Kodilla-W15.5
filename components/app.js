8e20a0e6caf1d634f4f8195288bcb5bb6ef28ecd
Radoslaw Podgorski <radoslaw.podgorski@gmail.com>
2018-03-26 22:42:14 +0100

    first commit

--- components/app.js
+++ components/app.js
@@ -10,28 +10,30 @@ App = React.createClass({
         };
     },
 
-    getGif: function(searchingText, callback) {                 // 1.
-        var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;  // 2.
-        var xhr = new XMLHttpRequest();                         // 3.
-        xhr.open('GET', url);
-        xhr.onload = function() {
-            if (xhr.status === 200) {
-                var data = JSON.parse(xhr.responseText).data;   // 4.
-                var gif = {                                     // 5.
-                    url: data.fixed_width_downsampled_url,
-                    sourceUrl: data.url
-                };
-                callback(gif);                                  // 6.
-            }
-        };
-        xhr.send();
+    getGifPromise: function(searchingText) {
+        return new Promise(function(resolve, reject) {
+            const xhr = new XMLHttpRequest(); 
+            xhr.onload = function() {
+                if (xhr.status === 200) {
+                    var data = JSON.parse(xhr.responseText).data; 
+
+                    var gif = { 
+                        url: data.fixed_width_downsampled_url,
+                        sourceUrl: data.url
+                    };
+                    resolve(gif)
+                } else {reject(new Error(this.statusText));}
+            };
+            xhr.open('GET', url + searchingText);
+            xhr.send();
+        });
     },
 
     handleSearch: function(searchingText) {                     // 1
         this.setState({
             loading: true                                       // 2
         });
-        this.getGif(searchingText, function(gif) {              // 3
+        this.getGifPromise(searchingText).then(function(gif) {              // 3
             this.setState({                                     // 4
                 loading: false,                                 // a
                 gif: gif,                                       // b
